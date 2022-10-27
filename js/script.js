1d7d243dcebf74b0d8fc246783db1dd189c7df8d
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-27 16:15:40 +0200

    starter pack

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+class Abbigliamento {
+    constructor(id, codprod, collezione, capo, modello, quantita, colore, prezzoivaesclusa, prezzoivainclusa, disponibile, saldo) {
+        this.id = id;
+        this.codprod = codprod;
+        this.collezione = collezione;
+        this.capo = capo;
+        this.modello = modello;
+        this.quantita = quantita;
+        this.colore = colore;
+        this.prezzoivaesclusa = prezzoivaesclusa;
+        this.prezzoivainclusa = prezzoivainclusa;
+        this.disponibile = disponibile;
+        this.saldo = saldo;
+    }
+    getSaldoCapo() {
+        return this.saldo;
+    }
+    getAcquistoCapo() {
+        return this.prezzoivainclusa;
+    }
+}
---
+++ ts/script.ts
@@ -0,0 +1,33 @@
+class Abbigliamento {
+    id:number;
+    codprod:number;
+    collezione:string;
+    capo:string;
+    modello:number;
+    quantita:number;
+    colore:string;
+    prezzoivaesclusa:number;
+    prezzoivainclusa:number;
+    disponibile:string;
+    saldo:number;
+    constructor(id:number, codprod:number, collezione:string, capo:string, modello:number, quantita:number, colore:string, 
+        prezzoivaesclusa:number, prezzoivainclusa:number, disponibile:string, saldo:number) {
+        this.id=id;
+        this.codprod=codprod;
+        this.collezione=collezione;
+        this.capo=capo;
+        this.modello=modello;
+        this.quantita=quantita;
+        this.colore=colore;
+        this.prezzoivaesclusa=prezzoivaesclusa;
+        this.prezzoivainclusa=prezzoivainclusa;
+        this.disponibile=disponibile;
+        this.saldo=saldo;
+    }
+    getSaldoCapo():number {
+        return this.saldo
+    }
+    getAcquistoCapo():number {
+        return this.prezzoivainclusa
+    }
+}
